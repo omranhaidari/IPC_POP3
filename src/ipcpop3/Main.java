c1f29f242bafa15964a5b96a21e3cf8da02f1799
THIBOUD PIERRE-ELLIOTT p1402690 <p1402690@univ-lyon1.fr>
2019-03-13 11:30:29 +0100

    Ajout StreamUtil + Mailbox + Mail

--- src/ipcpop3/Main.java
+++ src/ipcpop3/Main.java
@@ -6,30 +6,38 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
         try {
+            List<POP3Context> usersConnected = new ArrayList<>();
             ServerSocket serverSocket = new ServerSocket(8025);
 
             while(true) {
-                System.out.println("Attente de connexion...");
+                System.out.println("Attente de connexion... (" + usersConnected.size() + " users currently connected)");
                 Socket socket = serverSocket.accept();
                 System.out.println("Connexion établie");
 
-                ((Runnable) () -> {
-                    OutputStream out = null;
-                    try {
-                        out = socket.getOutputStream();
-                        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                        POP3Context context = new POP3Context(in, out);
-                        context.setState(new Authorization1State(context));
-                        context.run();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }).run();
+                POP3Context context = POP3Context.createContext(socket);
+                if(context != null) {
+                    usersConnected.add(context);
+                    ((Runnable) () -> {
+                        try {
+                            context.init();
+                            context.run();
+                        } catch (IOException e) {
+                            // Gère création du context et l'envoie de la première réponse
+                            e.printStackTrace();
+                        }
+                    }).run();
+                } else {
+                    // TODO Log l'erreur (user n'a pas pu se connecter)
+                }
+
+
             }
         } catch (IOException e) {
             e.printStackTrace();
