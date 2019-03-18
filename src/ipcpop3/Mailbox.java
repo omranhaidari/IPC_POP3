d12dfbeefffc5214ce57ce61148011f8d8ab9d03
Omran Haidari <omran.haidari@etu.univ-lyon1.fr>
2019-03-18 10:14:42 +0100

    Début ouverture d'un fichier pour stat

--- src/ipcpop3/Mailbox.java
+++ src/ipcpop3/Mailbox.java
@@ -1,5 +1,9 @@
 package ipcpop3;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,6 +13,20 @@ public class Mailbox {
     public Mailbox(String mailboxName) {
         // TODO Ouvrir le fichier de la boîte mail (avec gestion des erreurs)
 
+        try {
+            File file = new File("chemin_vers_mon_fichier");
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            try {
+                StreamUtil.readLine(reader);
+
+            } finally {
+                reader.close();
+            }
+
+        } catch (IOException ex) {
+            // erreur d'entrée/sortie ou fichier non trouvé
+            ex.printStackTrace();
+        }
         // TODO Charger tous les mails dans la liste
         mails = new ArrayList<>();
         
--- src/ipcpop3/TransactionState.java
+++ src/ipcpop3/TransactionState.java
@@ -1,5 +1,10 @@
 package ipcpop3;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class TransactionState extends POP3State {
     public TransactionState(POP3Context context) {
         super(context);
@@ -11,7 +16,28 @@ public class TransactionState extends POP3State {
 
     public void stat() {
         System.out.println("STAT Transaction");
-        // Stat
+        int nbMessage = 0;
+        // TODO : Récupération du nombre de messages par lecture du fichier
+        // TODO : Voir Mail pour la lecture d'un fichier et la récupération du nombre de .<CR><LF>
+        try {
+//            int nbLigne = 0;
+//            String ligne = null;
+            File file = new File("chemin_vers_mon_fichier");
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+//            try {
+//                while((ligne = reader.readLine()) != null) {
+//                    nbLigne++;
+//                }
+//            } finally {
+//                reader.close();
+//            }
+
+            long sizeMessage = file.length(); // taille du fichier en octets
+            System.out.println("+OK" + " " + nbMessage + " " + sizeMessage);
+        } catch (IOException ex) {
+            // erreur d'entrée/sortie ou fichier non trouvé
+            ex.printStackTrace();
+        }
     }
 
     public void retr() {
