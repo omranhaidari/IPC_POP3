9b28bd4e330d39e6c82c87086a181c2543b5abde
Axel <axel.bertrand2@etu.univ-lyon1.fr>
2019-03-18 11:07:08 +0100

    Ouverture des fichiers lockés pour la mailbox

--- src/ipcpop3/Mailbox.java
+++ src/ipcpop3/Mailbox.java
@@ -1,16 +1,31 @@
 package ipcpop3;
 
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.channels.FileLock;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Mailbox {
     private List<Mail> mails;
+    private InputStream in;
+    private OutputStream out;
+    private FileLock fli;
+    private FileLock flo;
 
     public Mailbox(String mailboxName) {
         // TODO Ouvrir le fichier de la boîte mail (avec gestion des erreurs)
 
         // TODO Charger tous les mails dans la liste
         mails = new ArrayList<>();
-        
+    }
+
+    public Mailbox(InputStream in, FileLock fli, OutputStream out, FileLock flo) {
+        this.in = in;
+        this.out = out;
+        this.fli = fli;
+        this.flo = flo;
+
+        this.mails = new ArrayList<>();
     }
 }
